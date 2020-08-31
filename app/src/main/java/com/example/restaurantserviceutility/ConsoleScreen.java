80e2a3dfe6a9fba32643befa7349b194aebbe946
vishal929 <48373882+vishal929@users.noreply.github.com>
2020-08-31 16:12:16 -0400

    Starting to work on internet functionality for communication between console,kitchen, and waiter

--- app/src/main/java/com/example/restaurantserviceutility/ConsoleScreen.java
+++ app/src/main/java/com/example/restaurantserviceutility/ConsoleScreen.java
@@ -8,11 +8,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.restaurantserviceutility.Networking.MyWifiReceiver;
 
 import RestaurantClasses.Service.Console;
 import RestaurantClasses.ServiceTools.Order;
@@ -30,6 +36,12 @@ public class ConsoleScreen extends AppCompatActivity implements MyTextInputDialo
 
     private Console console;
 
+    //internet functionality (we need to show up on the network as a console for kitchens, waiters to connect to)
+    WifiP2pManager manager;
+    WifiP2pManager.Channel channel;
+    BroadcastReceiver receiver;
+    IntentFilter intentFilter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +52,16 @@ public class ConsoleScreen extends AppCompatActivity implements MyTextInputDialo
         String name = savedInstanceState.getString("name");
         //making a console
         console = new Console(name);
+
+        //internet functionality
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+        receiver = new MyWifiReceiver(manager, channel, this);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
         //price text
         price = findViewById(R.id.consoleOrderTotalText);
---
+++ app/src/main/java/com/example/restaurantserviceutility/Networking/MyWifiReceiver.java
@@ -0,0 +1,51 @@
+package com.example.restaurantserviceutility.Networking;
+
+
+import android.app.Activity;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.os.Handler;
+
+/**
+ * Class that handles my wifi connections.
+ */
+public class MyWifiReceiver extends BroadcastReceiver {
+
+    private WifiP2pManager manager;
+    private Channel channel;
+    private Activity activity;
+
+    public MyWifiReceiver(WifiP2pManager manager, Channel channel,
+                          Activity activity) {
+        super();
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                // Wifi P2P is enabled
+            } else {
+                // Wi-Fi P2P is not enabled
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+}
